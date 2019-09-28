c2d42cb5d18df8af9841fb5af5fb9f3a4447e7ec
Michael M <m.migsch@gmail.com>
2019-09-29 00:20:53 +0200

    Create playing_with_decorators.py

---
+++ playing_with_decorators.py
@@ -0,0 +1,17 @@
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        print("I'm befor the function call!")
+        func(*args, **kwargs)
+        print("I'm after the function call!")
+    return wrapper
+
+
+@decorator
+def func(*args):
+    result = 0
+    for i in args:
+        result += i
+    print("Sum = ", result)
+
+
+func(1, 3, 45, 77)
